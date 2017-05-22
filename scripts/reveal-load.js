9503e401cd0d50bc602f85501e95b7609b582b7e
Aaron Moller <moller.aaron@gmail.com>
2017-05-22 12:09:23 -0400

    add scripts dir and reveal config

---
+++ scripts/reveal-load.js
@@ -0,0 +1,15 @@
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize({
+
+});
+
+Reveal.configure({
+    keyboard: {
+        37: Reveal.prev, // left
+        38: Reveal.prev, // up
+        39: Reveal.next, // right
+        40: Reveal.next, // down
+    }
+});
